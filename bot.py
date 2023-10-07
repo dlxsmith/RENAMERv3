f8a12c72151862ddcc2d050b167f8ffff616553c
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2023-10-08 00:00:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,11 +4,11 @@ import os
 
 from plugins.cb_data import app as Client2
 
-TOKEN = os.environ.get("TOKEN", "")
+TOKEN = os.environ.get("TOKEN", "6640601389:AAEQaqs79FcPTW5YMdwNZv_rhV7M7urWE1k")
 
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "23374527"))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "5a48ec466dfa7df47e2a109ed40019bc")
 
 STRING = os.environ.get("STRING", "")
 
