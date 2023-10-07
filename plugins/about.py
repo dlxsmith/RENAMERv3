6c6b14acff1f4b9c5c702e080e420cde5812be0a
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2023-10-08 00:04:56 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -10,4 +10,4 @@ async def start(client,message):
 	data = find_one(int(botid))
 	total_rename = data["total_rename"]
 	total_size = data["total_size"]
-	await message.reply_text(f"Total User:- {total_user()}\nBot :- @Rename_658_bot\nBuy Subscription :- @GreyMatter_Bot\nSubscribe :- https://youtube.com/@GreyMattersYT\n\nTotal Renamed File :-{total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} ",quote=True)
+	await message.reply_text(f"Total User:- {total_user()}\nBot :- @dlx_renamerbot\nBuy Subscription :- @dlx_smith\nSubscribe :- https://t.me/animes_in_30mb\n\nTotal Renamed File :-{total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} ",quote=True)
